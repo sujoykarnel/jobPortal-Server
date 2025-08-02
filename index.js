da5573419f12b24f0a048b92d4194c17ac1fd7cf
Sujoy Das <sujoykarnel@gmail.com>
2025-08-02 10:29:45 +0600

    Updated

--- index.js
+++ index.js
@@ -72,6 +72,13 @@ async function run() {
         .send({ success: true });
     });
 
+    app.post('/logout', (req, res) => {
+      res.clearCookie('token', {
+        httpOnly: true,
+        secure: false
+      }).send({success: true})
+    })
+
     // jobs related apis
     app.get("/jobs", async (req, res) => {
       const email = req.query.email;
